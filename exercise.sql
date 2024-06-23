7eb0dc7223ac8382d3c5fef34725ee79fafd4f71
SophieNguyen311 <20104432d@connect.polyu.hk>
2024-06-23 23:38:20 +0800

    Update exercise.sql

--- exercise.sql
+++ exercise.sql
@@ -1,1 +1,57 @@
+--ex 1  
+select name from city 
+where population > 120000 
+and countrycode = 'USA'
+--ex 2
+Select * from city 
+where countrycode = 'JPN'
+--ex3
+select city, state from station
+--ex4
+select distinct city 
+  from station 
+where city like 'A%'  
+  or city like 'E%'  
+  or city like 'I%' 
+  or city like 'O%' 
+  or city like 'U%'
+--ex5
+Select distinct city from station 
+where city like '%a' 
+  or city like '%e'  
+  or city like '%i' 
+  or city like '%o'  
+  or city like '%u'
+--ex6 Query the list of CITY names from STATION that do not start with vowels
+select distinct city 
+  from station
+where city not like 'A%'
+and city not like 'E%'
+and city not like 'I%'
+and city not like 'O%'
+and city not like 'U%'
+--ex 7 
+select name from employee 
+order by name asc
+--ex8 
+select name from employee 
+where salary >2000
+and months < 10 
+order by employee_id asc
+--ex9 
+select product_id from products
+where low_fats = 'Y'
+and recyclable = 'Y'
+--ex10
+select name from customer 
+where referee_id <> 2
+or referee_id is NULL 
+--ex11
+select name, population, area from World
+where area >= 3000000
+or population >= 25000000
+--ex12
+
+
+
 
